b086d2de04d8b0e62928ba7707c6229afc1a8bee
Maurizio Pillitu <m@session.it>
2011-05-06 15:16:50 +0200

    new images

--- scripts/site.search.js
+++ scripts/site.search.js
@@ -1,5 +1,5 @@
 //Enter domain of site to search.
-var domainroot="www.javascriptkit.com"
+var domainroot="www.paneeolio.com"
 
 function Gsitesearch(curobj){
 curobj.q.value="site:"+domainroot+" "+curobj.qfront.value
